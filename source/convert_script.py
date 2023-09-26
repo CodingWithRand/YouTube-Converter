72ba3c27bd10b73acfad6ebfe7143c576b6e7f49
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2023-09-26 22:22:07 +0700

    v1.3 (2) - Currently unavailable

--- source/convert_script.py
+++ source/convert_script.py
@@ -9,7 +9,7 @@ import re
 import traceback
 import threading
 import addition_script as adds
-
+from time import sleep
 
 
 def check_availability(asset):
@@ -71,7 +71,12 @@ def file_sorting(asset, filedir, file_name, ext, ign):
 
 
 def one_download(link, mode, directory, root):
+    sync_event = threading.Event()
+    stop_flag = threading.Event()
+    max_percentage = 1000
+
     progression = DoubleVar()
+    progressText = StringVar()
     loading = Toplevel(root)
     loading.title("YouTube Converter")
     loading.iconbitmap("icon.ico")
@@ -80,50 +85,113 @@ def one_download(link, mode, directory, root):
     loading.geometry(adds.sizing_positioning((1000, 200), adds.win_center_pos(loading, (1000,200))))
     root.attributes("-disabled", True)
     body = Frame(loading)
-    progressionText = Label(body, text='Starting Task...', font=('Arial', 15))
-    progressionBar = ttk.Progressbar(body, variable=progression, maximum=100)
-    progressionText.grid(row=0)
-    progressionBar.grid(row=1)
-    body.grid_rowconfigure()
-    body.pack 
-    def work():
-        f = None
+    progressionText = Label(body, textvariable=progressText, font=('Arial', 15))
+    progressionBar = ttk.Progressbar(body, variable=progression, maximum=max_percentage)
+    progressionText.grid(row=0, column=0, pady=(30, 10))
+    progressionBar.grid(row=1, column=0, pady=(20, 60), padx=100, sticky=EW)
+    body.grid_columnconfigure(0, weight=1)
+    body.grid_rowconfigure(0, weight=1)
+    body.pack(fill="both", expand=True)
+
+    def fake_progressing(exceptional_percentage):
+        def body():
+            while not stop_flag.is_set():
+                progression.set(progression.get() + 1)
+                if progression.get() == exceptional_percentage: break
+                sleep(0.05)
+            sleep(1)
+            return
+        fp = threading.Thread(target=body)
+        fp.start()
+
+    def raiseErr(errCode):
+        global err_code
+        loading.destroy()
+        root.attributes("-disabled", False)
+        err_code = errCode
+        sync_event.set()
+    
+    def real_progressing(showingTask, progress, holdingTime, job, kwargs=None):
+        stop_flag.clear()
+        progressText.set(showingTask)
+        fake_progressing(progress)
+        job(kwargs)
+        sleep(holdingTime)
+        stop_flag.set()
+        progression.set(progress)
+
+    def work(): 
+        global f
+        global video
         try:
-            f = YouTube(link)
-        except:
-            mbox.showerror("System Error", "Invalid YouTube Link!")
+            def retrieve_link(*args, **kwargs): 
+                global f
+                f = YouTube(link)
+            real_progressing("Retrieving YouTube link...", 100, 2, retrieve_link)
+        except Exception:
+            raiseErr("001")
             return
-        if directory == "":
-            mbox.showerror("System Error", "Invalid File Path!")
+
+        if directory == "": 
+            raiseErr("002")
             return
+
         output_ext = None
         if mode.lower() == "video":
             output_ext = ".mp4"
         elif mode.lower() == "audio":
             output_ext = ".mp3"
-        check_availability(f)
-        video = f.streams.get_highest_resolution()
         try:
-            file_dir = directory
-            f.title = replace_unsupported_char(f.title)
-            file_sorting(video, file_dir, f.title, output_ext, False)
-            # progressionBar.pack()
-            # progressionText.pack()
-        except WindowsError:
-            print("File's name doesn't support, but it downloaded successful anyway.")
-            mbox.showwarning("System Warning", "File's name doesn't support, your file has been downloaded! (Code: 3009)")
-        except FileExistsError:
-            print("The file is already exist!")
-            mbox.showerror("System Error", "The file is already exist!")
-        except PermissionError:
-            print("You don't have permission to download into this folder")
-            mbox.showerror("System Error", "You don't have permission to download into this folder!")
-        except Exception as e:
-            traceback_str = traceback.format_exc()
-            print("An Error occurred")
-            mbox.showerror("System Error", "An unknown error occurred!")
-            mbox.showerror("System Error", "Detail: " + str(e) if e is not None else "No error details available.")
-            mbox.showerror("System Error", f"{traceback_str}")
+            def get_video(yt_v):
+                global video
+                check_availability(yt_v)
+                video = f.streams.get_highest_resolution()
+            real_progressing("Check for the video availability...", 200, 4, get_video, {'yt_v': f})
+        except: 
+            raiseErr("003")
+            return
+
+        # try:
+        #     file_dir = directory
+        #     f.title = replace_unsupported_char(f.title)
+        #     file_sorting(video, file_dir, f.title, output_ext, False)
+        #     # progressionBar.pack()
+        #     # progressionText.pack()
+        # except WindowsError:
+        #     print("File's name doesn't support, but it downloaded successful anyway.")
+        #     mbox.showwarning("System Warning", "File's name doesn't support, your file has been downloaded! (Code: 3009)")
+        # except FileExistsError:
+        #     print("The file is already exist!")
+        #     mbox.showerror("System Error", "The file is already exist!")
+        # except PermissionError:
+        #     print("You don't have permission to download into this folder")
+        #     mbox.showerror("System Error", "You don't have permission to download into this folder!")
+        # except Exception as e:
+        #     traceback_str = traceback.format_exc()
+        #     print("An Error occurred")
+        #     mbox.showerror("System Error", "An unknown error occurred!")
+        #     mbox.showerror("System Error", "Detail: " + str(e) if e is not None else "No error details available.")
+        #     mbox.showerror("System Error", f"{traceback_str}")
+
+    def catch_error():
+        global err_code
+        sync_event.wait()
+        while err_code:
+            if err_code == "001": 
+                mbox.showerror("System Error", f"Invalid YouTube Link! (Error Code: {err_code})")
+                break
+            elif err_code == "002": 
+                mbox.showerror("System Error", f"Invalid File Path! (Error Code: {err_code})")
+                break
+            elif err_code == "003":
+                mbox.showerror("System Error", f"The video is currently unavailable, We're sorry about that! (Error Code: {err_code})")
+                break
+        else: mbox.showinfo("System", "Your conversion is successfully completed!")
+    
+    convert = threading.Thread(target=work)
+    convert.start()
+    catch = threading.Thread(target=catch_error)
+    catch.start()
 
 
 def playlist_download(link, mode, pldir):
